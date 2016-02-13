0303e5f7d8d0253a562b02d936f139f008744b78
Maigret Aurélien <dewep.net@gmail.com>
2016-02-13 15:34:12 +0000

    Improvements

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import random
+import math
 import plotly
 import plotly.graph_objs as go
+
+pop_size = 500
+min_coefficient = -1000.0
+max_coefficient = min_coefficient * -1
+coefs_size = 6
+proba_crossover = 0.85
+proba_mutation = 0.0001
+mutation_rate = 0.00001
+nb_turns = 3000
+
 
 tab_x = list()
 tab_y = list()
 real_tab_x = list()
 real_tab_y = list()
-level = 1
-nb_turns = 100000000
 
 with open("data.in") as input_file:
     for line in input_file:
@@ -17,6 +26,10 @@ with open("data.in") as input_file:
             real_tab_x.append(float(numbers[0]))
             real_tab_y.append(float(numbers[1]))
 
+tab_x = real_tab_x
+tab_y = real_tab_y
+
+"""
 def find_average_y_value(x):
     for index in range(len(real_tab_x)):
         if real_tab_x[index] > x:
@@ -35,6 +48,7 @@ while x <= 900:
         x += 7
     else:
         x += 50
+"""
 
 def compute_value(coefs, x):
     return coefs[0] + coefs[1] * x + coefs[2] * x * x + coefs[3] * x * x * x + coefs[4] * x * x * x * x + coefs[5] * x * x * x * x * x
@@ -44,7 +58,6 @@ class solutionIndividual:
     def __init__(self, coefs):
         self.coefs = coefs
         self._fitness = 0
-        self._mutation()
         self._compute_fitness()
 
     @property
@@ -52,48 +65,60 @@ class solutionIndividual:
         return self._fitness
 
     def _compute_fitness(self):
+        self._fitness = 0
         for index in range(len(tab_x)):
-            self._fitness += abs(tab_y[index] - compute_value(self.coefs, tab_x[index]))
+            tmp = tab_y[index] - compute_value(self.coefs, tab_x[index])
+            self._fitness += tmp * tmp
+        self._fitness = math.sqrt(self._fitness)
 
-    def _mutation(self):
-        if random.random() < 0.4:
-            for index in range(len(self.coefs)):
-                if random.random() < 0.4:
-                    level_max = level * (len(self.coefs) - index)
-                    self.coefs[index] += random.randint(-level, level)
-                elif random.random() < 0.4:
-                    self.coefs[index] *= -1
+    def mutation(self):
+        index = random.randint(0, len(self.coefs) - 1)
+        mutation_value = abs(mutation_rate * self.coefs[index])
+        if random.random() < 0.5:
+            mutation_value *= -1
+        self.coefs[index] += mutation_value
 
     def crossover(self, other):
-        point = random.randint(0, len(self.coefs) - 1)
-        genome1 = self.coefs[:point] + other.coefs[point:]
-        genome2 = other.coefs[:point] + self.coefs[point:]
-        child1 = solutionIndividual(genome1)
-        child2 = solutionIndividual(genome2)
-        return child1, child2
+        coefs1 = list(self.coefs)
+        coefs2 = list(self.coefs)
+        coefs3 = list(self.coefs)
+        for index in range(len(self.coefs)):
+            coefs1[index] = 0.5 * self.coefs[index] + 0.5 * other.coefs[index]
+            coefs2[index] = 1.5 * self.coefs[index] - 0.5 * other.coefs[index]
+            coefs3[index] = -0.5 * self.coefs[index] + 1.5 * other.coefs[index]
+        childs = [solutionIndividual(coefs1), solutionIndividual(coefs2), solutionIndividual(coefs3)]
+        return sorted(childs, key=lambda x: x.fitness)[:2]
+
+def breeding(ind1, ind2):
+    if random.random() < proba_crossover:
+        ind1, ind2 = ind1.crossover(ind2)
+    if random.random() < proba_mutation:
+        ind1.mutation()
+    if random.random() < proba_mutation:
+        ind2.mutation()
+    return ind1, ind2
 
 def next_pop(pop):
-    pos = int(len(pop) / 2)
-    pop = sorted(pop, key=lambda x: x.fitness)[:pos]
-    best = pop[0]
-    i = 0
-    while i + 1 < pos:
-        pop.extend(pop[i].crossover(pop[i + 1]))
-        i += 2
-    return best, pop
-
+    pop = sorted(pop, key=lambda x: x.fitness)
+    new_pop = list()
+    new_pop.append(pop[0])
+    for i in range(math.floor(pop_size / 2)):
+        new_pop.extend(breeding(pop[i * 2], pop[i * 2 + 1]))
+    return new_pop
 
 def random_solution():
-    s = [0, 0, 0, 0, 0, 0]
-    for index in range(len(s)):
-        s[index] = random.randint(-level, level)
+    s = list()
+    for _ in range(coefs_size):
+        s.append(random.uniform(min_coefficient, max_coefficient))
     return s
 
-best = None
-pop = [solutionIndividual(random_solution()) for _ in range(20)]
-for _ in range(nb_turns):
-    best, pop = next_pop(pop)
+pop = [solutionIndividual(random_solution()) for _ in range(pop_size)]
+for turn in range(nb_turns):
+    if turn % (math.floor(nb_turns / 100)) == 0:
+        print("%3d%%: %s %f" % (math.floor(turn / nb_turns * 100), str(pop[0].coefs), pop[0].fitness))
+    pop = next_pop(pop)
 
+best = pop[0]
 print(best.coefs)
 print(best.fitness)
 
--- plot.py
+++ plot.py
@@ -3,6 +3,14 @@ import plotly.graph_objs as go
 
 tab_x = list()
 tab_y = list()
+tab_x2 = list()
+tab_y2 = list()
+
+best = [-0.0000015230981995981592, 5000.000000176404, 4.999999998293433, -61.99999999999368, 0.9999999999999908, -0.0009999999999999955]
+best = [0.0, 5000.0, 5.0, -62.0, 1.0, -0.001]
+
+def compute_value(coefs, x):
+    return coefs[0] + coefs[1] * x + coefs[2] * x * x + coefs[3] * x * x * x + coefs[4] * x * x * x * x + coefs[5] * x * x * x * x * x
 
 with open("data.in") as input_file:
     for line in input_file:
@@ -11,13 +19,9 @@ with open("data.in") as input_file:
         if len(numbers) == 2:
             tab_x.append(float(numbers[0]))
             tab_y.append(float(numbers[1]))
-            #target[float(numbers[0])] = float(numbers[1])
 
-trace = go.Scatter(
-    x = tab_x,
-    y = tab_y
-)
+for index in range(len(tab_x)):
+    tab_x2.append(tab_x[index])
+    tab_y2.append(compute_value(best, tab_x[index]))
 
-data = [trace]
-
-plotly.offline.plot(data, filename='compare-results')
+plotly.offline.plot([go.Scatter(x=tab_x, y=tab_y), go.Scatter(x=tab_x2, y=tab_y2)], filename='compare-results-tmp.html')
